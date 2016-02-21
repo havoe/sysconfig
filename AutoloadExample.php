06e73245b479c3e6e7b540842b1087c0e3977b14
havoe <4759017@qq.com>
2016-02-21 18:23:28 +0800

    first commit

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace havoe\sysconfig;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
